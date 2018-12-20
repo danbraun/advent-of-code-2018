95ec3936bec6a2bf6ea485613d62d6c74ecd9c9a
danielbraun <daniel@vectorandink.com>
2018-12-20 10:20:43 -0600

    updates to formatting on day02 index and new stuff started on day03

--- day02/index.js
+++ day02/index.js
@@ -25,7 +25,7 @@ const getAnswer1 = input => {
     });
 
     const charCount = Object.values(matchedObj);
-    console.log('charCount:', charCount);
+    // console.log('charCount:', charCount);
     let foundOccuranceOf2 = false;
     let foundOccuranceOf3 = false;
     charCount.forEach(element => {
@@ -43,8 +43,8 @@ const getAnswer1 = input => {
 };
 //getAnswer1(input);
 
-const getAnswer2 = (input) => {
-  input.forEach( (box, i) => {
+const getAnswer2 = input => {
+  input.forEach((box, i) => {
     // looping though every box id, first get a list of
     // box ids that don't include the one we're going to check
     // all others against
@@ -52,13 +52,13 @@ const getAnswer2 = (input) => {
     everyOtherBox.splice(i, 1);
     // using .some so that we can stop the loop as soon as
     // checkUs finds an answer and returns true
-    everyOtherBox.some( (otherBox) => {
+    everyOtherBox.some(otherBox => {
       // check the box id we're currently on in the forEach loop
       // against one of the other box ids in the whole input list of box ids
       return checkUs(box, otherBox);
-    })
-  })
-}
+    });
+  });
+};
 
 const checkUs = (box1, box2) => {
   // break each of the box ids into an array of characters
@@ -72,7 +72,7 @@ const checkUs = (box1, box2) => {
   // one.
   // Check each character from box1 with against each character
   // in box2 in the same position
-  box1arr.forEach( (char, i) => {
+  box1arr.forEach((char, i) => {
     if (char === box2arr[i]) {
       matchedChars.push(char);
     }
@@ -80,15 +80,15 @@ const checkUs = (box1, box2) => {
   // if the cumulative total of matchedChars is equal in length of
   // a box id (which are all the same) minus 1, then it satifies the
   // puzzle's test requirement of matching all characters from one box
-  // id with another's in the same positions except for one. 
+  // id with another's in the same positions except for one.
   // This will return the answer twice, as the second of the matched
   // will again match the first as we loop though all box ids in the
   // function calling this one. Inefficent perhaps...
   if (matchedChars.length == box1arr.length - 1) {
-    console.log(`Answer 2 is: ${matchedChars.join("")}`);
+    console.log(`Answer 2 is: ${matchedChars.join('')}`);
     return true;
   }
   return false;
-}
+};
 
 getAnswer2(input);
--- day03/index.js
+++ day03/index.js
@@ -5,61 +5,73 @@ const input = fs
   .toString()
   .split('\n');
 
-const getAnswer1 = (input) => {
-    const fabricMatrix = returnFabricMatrix(8);
-    const topLeft = input[0].split('@ ')[1].split(':')[0].split(',');
-    const widthHeight = input[0].split(': ')[1].split('x');
-    
-    const leftMove = Number(topLeft[0]);
-    const topMove = Number(topLeft[1]);
-    const width = Number(widthHeight[0]);
-    const height = Number(widthHeight[1]);
+const getAnswer1 = input => {
+  // return a matrix (i think that's the correct term?) of objects
+  // to represent a grid with locations and settings
+  const fabricMatrix = returnFabricMatrix(8);
+  // for testing, return values from a test string: '#1 @ 1,3: 4x4'
+  const topLeft = input[0]
+    .split('@ ')[1]
+    .split(':')[0]
+    .split(',');
+  const widthHeight = input[0].split(': ')[1].split('x');
 
-    const foundLocationIds = [];
+  // from all the splitting above, we now have a left / top (leftMove / topMOve)
+  // corner to begin a claimed rectangle. Additionally we have a width
+  // and height for the rectangle
+  const leftMove = Number(topLeft[0]);
+  const topMove = Number(topLeft[1]);
+  const width = Number(widthHeight[0]);
+  const height = Number(widthHeight[1]);
 
-    for (let i = 0; i < width; i++) {
-        const foundLocation = fabricMatrix.find( (locationObj) => {
-            const x = leftMove+1;
-            const y = topMove+1
-            return locationObj.x == x+i && locationObj.y == y;
-        })
-        foundLocationIds.push(foundLocation.id);
-    }
-    for (let i = 0; i < height; i++) {
-        const foundLocation = fabricMatrix.find( (locationObj) => {
-            const x = leftMove+1;
-            const y = topMove+1
-            return locationObj.x == x && locationObj.y == y+1;
-        })
-        foundLocationIds.push(foundLocation.id);
-    }
-    console.log(foundLocationIds);
-    foundLocationIds.forEach( (id) => {
-        fabricMatrix[id].claimCount++;
-    })
-    console.log(fabricMatrix);
-    
-}
+  // based on our input, well find a list of 'claimed' objects
+  // and push them into here
+  const foundLocationIds = [];
 
-const testInput = [
-    '#1 @ 1,3: 4x4'
-];
-const returnFabricMatrix = (inches) => {
-    const area = inches*inches;
-    const fabricMatrix = [];
-    let yIncrementor = 1;
-    for ( let i = 0; i<area; i++) {
-        locationObj = {
-            claimCount: 0,
-            id: i
-        }
-        locationObj.x = i%inches+1;
-        locationObj.y = yIncrementor;
-        if ((i+1)%inches == 0) {
-            yIncrementor++;
-        }
-        fabricMatrix.push(locationObj);
+  // loop through a total identified by width and use Array.find
+  // to find the top row of the claimed rectangle
+  for (let i = 0; i < width; i++) {
+    // with each iteration of width, use Array.find to loop though the
+    // entire fabricMatrix array and find location objects in the matrix
+    // that have x's and y's that match x+i (which increases with each iteration
+    // of the above for loop) and y (which for now stays the same)
+    const foundLocation = fabricMatrix.find(locationObj => {
+      // locationObj.x and locationObj.y are 1 based so add one to leftMove / topMove
+      const x = leftMove + 1 + i;
+      const y = topMove + 1;
+      return locationObj.x == x && locationObj.y == y;
+    });
+    //console.log(`foundLocation.id: ${foundLocation.id}`);
+    foundLocationIds.push(foundLocation.id);
+  }
+
+  foundLocationIds.forEach(id => {
+    fabricMatrix[id].claimCount++;
+  });
+  //console.log(fabricMatrix);
+};
+
+// Find all coords that should be claimed?
+const coordsToClaim = [];
+for (let i = 0; i < width; i++) {}
+
+const testInput = ['#1 @ 1,3: 4x4'];
+const returnFabricMatrix = inches => {
+  const area = inches * inches;
+  const fabricMatrix = [];
+  let yIncrementor = 1;
+  for (let i = 0; i < area; i++) {
+    locationObj = {
+      claimCount: 0,
+      id: i
+    };
+    locationObj.x = (i % inches) + 1;
+    locationObj.y = yIncrementor;
+    if ((i + 1) % inches == 0) {
+      yIncrementor++;
     }
-    return fabricMatrix;
-}
-getAnswer1(testInput)
+    fabricMatrix.push(locationObj);
+  }
+  return fabricMatrix;
+};
+getAnswer1(testInput);
